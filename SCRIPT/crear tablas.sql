ae29664c451a684e1d879da48426997eff5aff64
ailingranados <126365853+ailingranados@users.noreply.github.com>
2024-09-05 16:06:04 -0600

    Creacion de tablas
    Creacion de tablas principales en mysql

---
+++ SCRIPT/crear tablas.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE LetsTalk;
+
+USE LetsTalk;
+
+/************************************************************************************TABLAS DE USUARIO******/
+CREATE TABLE Usuario (
+    Id 				INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre 			VARCHAR(50) NOT NULL,
+    Apellido 		VARCHAR(50) NOT NULL,
+    Correo 			VARCHAR(100) NOT NULL UNIQUE,
+    Contraseña 		VARCHAR(50) NOT NULL,
+    Fecha_nacimiento	DATE NOT NULL
+);
+
+CREATE TABLE Estado_Usuario(
+	Id 				INT AUTO_INCREMENT PRIMARY KEY,
+	Estado 			TINYINT(1) NOT NULL,
+	Fecha_registro 	DATETIME NOT NULL,
+	Fecha_baja 		DATETIME
+);
+
+/*************************************************************************************TABLAS DE LISTBOX******/
+CREATE TABLE Categoria(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Nombre 				VARCHAR(50)
+);
+
+CREATE TABLE Plataforma(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Nombre 				VARCHAR(50)
+);
+
+/*************************************************************************************TABLAS DE RESEÑAS******/
+CREATE TABLE Libros(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Titulo 				VARCHAR(50),
+Author 				VARCHAR(50),
+Editorial 			VARCHAR(50),
+ISBN 				VARCHAR(15),
+Categoria			INT,
+
+/*LLAVES FORANEAS*/
+
+ CONSTRAINT FK_Categoria_Libros
+	FOREIGN KEY (Categoria) 
+		REFERENCES Categoria(Id)
+);
+
+CREATE TABLE Peliculas(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Titulo 				VARCHAR(50),
+Director 			VARCHAR(50),
+Actor_1 			VARCHAR(50),
+Actor_2 			VARCHAR(50),
+Duracion			INT,
+Categoria			INT,
+Plataforma			INT,
+
+/*LLAVES FORANEAS*/
+ CONSTRAINT FK_Categoria_Peliculas
+	FOREIGN KEY (Categoria) 
+		REFERENCES Categoria(Id),
+	
+ CONSTRAINT FK_Plataforma_Peliculas
+	FOREIGN KEY (Plataforma) 
+		REFERENCES Plataforma(Id)   
+);
+
+CREATE TABLE Series(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Titulo 				VARCHAR(50),
+Actor_1 			VARCHAR(50),
+Actor_2 			VARCHAR(50),
+Finalizada 			TINYINT(1) NOT NULL,
+Temporadas			INT,
+Capitulos			INT,
+Plataforma			INT,
+Categoria			INT,
+
+
+/*LLAVES FORANEAS*/
+ CONSTRAINT FK_Categoria_Series
+	FOREIGN KEY (Categoria) 
+		REFERENCES Categoria(Id),
+        
+ CONSTRAINT FK_Plataforma_Series
+	FOREIGN KEY (Plataforma) 
+		REFERENCES Plataforma(Id)        
+);
+
+/*************************************************************************************TABLAS DE RELACIONES******/
+
+CREATE TABLE Usuario_Libros(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Usuario				INT,
+Libro				INT,
+Calificacion		INT,
+Fecha				DATETIME NOT NULL,
+
+/*LLAVES FORANEAS*/
+ CONSTRAINT FK_IdUsuario_Libros
+	FOREIGN KEY (Usuario) 
+		REFERENCES Usuario(Id),
+        
+ CONSTRAINT FK_IdLibro
+	FOREIGN KEY (Libro) 
+		REFERENCES Libros(Id)        
+);
+
+CREATE TABLE Usuario_Peliculas(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Usuario				INT,
+Pelicula			INT,
+Calificacion		INT,
+Fecha				DATETIME NOT NULL,
+
+/*LLAVES FORANEAS*/
+ CONSTRAINT FK_IdUsuario_Pelicula
+	FOREIGN KEY (Usuario) 
+		REFERENCES Usuario(Id),
+        
+ CONSTRAINT FK_IdPelicula
+	FOREIGN KEY (Pelicula) 
+		REFERENCES Peliculas(Id)        
+);
+
+CREATE TABLE Usuario_Series(
+Id 					INT AUTO_INCREMENT PRIMARY KEY,
+Usuario				INT,
+Serie				INT,
+Calificacion		INT,
+Fecha				DATETIME NOT NULL,
+
+/*LLAVES FORANEAS*/
+ CONSTRAINT FK_IdUsuario_Serie
+	FOREIGN KEY (Usuario) 
+		REFERENCES Usuario(Id),
+        
+ CONSTRAINT FK_IdSerie
+	FOREIGN KEY (Serie) 
+		REFERENCES Series(Id)        
+);
